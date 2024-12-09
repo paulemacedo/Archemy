d0f9ab8a06fff1fd9dc82292e82c9b8582c6fae1
Paule Macedo <Prmdm@hotmail.com>
2024-12-09 12:23:55 -0300

    refactor: comment out color constants in helper script

--- scripts/helper.sh
+++ scripts/helper.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Cores ANSI para formatação de saída
-readonly COLOR_RED='\033[0;31m'
-readonly COLOR_GREEN='\033[0;32m'
-readonly COLOR_YELLOW='\033[0;33m'
-readonly COLOR_BLUE='\033[0;34m'
-readonly COLOR_MAGENTA='\033[0;35m'
-readonly COLOR_CYAN='\033[0;36m'
-readonly COLOR_RESET='\033[0m'
+#readonly COLOR_RED='\033[0;31m'
+#readonly COLOR_GREEN='\033[0;32m'
+#readonly COLOR_YELLOW='\033[0;33m'
+#readonly COLOR_BLUE='\033[0;34m'
+#readonly COLOR_MAGENTA='\033[0;35m'
+#readonly COLOR_CYAN='\033[0;36m'
+#readonly COLOR_RESET='\033[0m'
 
 # Função para exibir mensagens coloridas
 print_message() {
